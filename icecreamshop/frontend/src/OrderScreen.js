a797a6593230bc08b948c0dd129b82630141c381
matthieu hedin <hedin.matthieu75@gmail.com>
2023-05-07 20:00:03 +0100

    setup react

---
+++ icecreamshop/frontend/src/OrderScreen.js
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
+function OrderScreen() {
+  const [flavors, setFlavors] = useState([]);
+  console.log("flavors", flavors)
+  useEffect(() => {
+  axios.get('http://localhost:8000/flavors')
+    .then(response => {
+      setFlavors(response.data);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+  }, [])
+
+  return (
+    <div>
+      <h1>Enter your email</h1>
+    </div>
+  );
+}
+
+export default OrderScreen;
--- icecreamshop/icecreamshop/settings.py
+++ icecreamshop/icecreamshop/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "django_extensions",
+    "corsheaders",
     "rest_framework",
     "orders",
     "icecream_tubs",
@@ -58,9 +59,7 @@ ROOT_URLCONF = "icecreamshop.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [
-            os.path.join(BASE_DIR, "templates"),
-        ],
+        "DIRS": [os.path.join(BASE_DIR, 'frontend', 'build')],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -122,7 +121,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "/static/"
-STATICFILES_DIRS = [BASE_DIR / "static"]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'frontend', 'build', 'static')
+]
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = "media/"
